caefde39b1079d4a1b3bc2ffad18b34b27fce1c5
tristanphips <tristan.phipson@bbd.co.za>
2024-03-05 22:45:39 +0200

    initial/setup: added templates for the client, game manager and game state classes.

---
+++ src/main/java/com/freethebeans/Client.java
@@ -0,0 +1,8 @@
+package com.freethebeans;
+
+public class Client {
+    public static void main(String[] args) {
+        GameManager gameManager = new GameManager();
+        gameManager.runGame();
+    }
+}

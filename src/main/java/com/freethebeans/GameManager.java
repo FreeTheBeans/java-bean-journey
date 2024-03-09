2ecf38c30ad57f0bc10ee34e725b6ce7df281e2e
tristanphips <tristan.phipson@bbd.co.za>
2024-03-09 23:27:31 +0200

    feature/client-game-loop: added game loop and requests for state information.

--- src/main/java/com/freethebeans/GameManager.java
+++ src/main/java/com/freethebeans/GameManager.java
@@ -1,48 +1,48 @@
 package com.freethebeans;
 
 import java.util.Scanner;
-
-// ==============
-/**
- * This will serve as a guide to create the
- * GameManager with connections to the server
- * through the API. References here to "server"
- * are meant to be replaced.
- */
-// ==============
+import org.springframework.web.client.RestTemplate;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class GameManager {
-    // private Server server;
+    private final String SERVER_ENDPOINT = "http://localhost";
+    private final int SERVER_PORT = 31415;
     private Scanner scanner;
 
     GameManager() {
-        // server = new Server();
         scanner = new Scanner(System.in);
     }
 
     public void runGame() {
-        int currentState = 0;
+        String request = String.format("%s:%d/api/ping", SERVER_ENDPOINT, SERVER_PORT);
+        String response = new RestTemplate().getForObject(request, String.class);
+        JSONObject responseJSON = new JSONObject(response);
+        System.out.println("Response from Server: " + responseJSON.getString("message"));
+
+        String currentStateID = "dummyState";
 
         System.out.println("Welcome to Bean Escape Simulator!");
         System.out.println("Press enter to begin");
         scanner.nextLine();
 
         while (true) {
-            // GameState gameState = server.getGameState(currentState);
-            // System.out.println(gameState.getContext());
+            GameState currentState = getGameState(currentStateID);
+            String[] currentStateOptions = currentState.getOptions();
+            String[] currentStateTransitions = currentState.getTransitions();
+            System.out.println(currentState.getContext());
 
-            // for (int i = 0; i < gameState.getOptions().length; i++) {
-            // System.out.println((i + 1) + ") " + gameState.getOptions()[i]);
-            // }
+            for (int i = 0; i < currentStateOptions.length; i++) {
+                System.out.println((i + 1) + ") " + currentStateOptions[i]);
+            }
 
             System.out.println("Select your next move:");
             System.out.print("> ");
             String input = scanner.nextLine();
 
             if (!input.equals("q")) {
-                int choice = Integer.parseInt(input);
-                // int nextStateID = gameState.getTransitions()[choice - 1];
-                // currentState = nextStateID;
+                int choiceNumber = Integer.parseInt(input);
+                currentStateID = currentStateTransitions[choiceNumber - 1];
                 // if (gameState.isEndState()) {
                 // System.out.println("Congratulations! You have escaped!");
                 // break;
@@ -55,4 +55,36 @@ public class GameManager {
 
         scanner.close();
     }
+
+    private GameState getGameState(String stateName) {
+        String context = "";
+        String[] stateOptions = null;
+        String[] stateTransitions = null;
+        try {
+            String request = String.format("%s:%d/api/state/%s", SERVER_ENDPOINT, SERVER_PORT, stateName);
+            String response = new RestTemplate().getForObject(request, String.class);
+
+            JSONObject responseJSON = new JSONObject(response);
+            String message = responseJSON.getString("message");
+
+            JSONObject innerJSON = new JSONObject(message);
+            context = innerJSON.getString("context");
+            JSONArray optionsArray = innerJSON.getJSONArray("options");
+            JSONArray transitionsArray = innerJSON.getJSONArray("transitions");
+
+            int optionsLength = optionsArray.length();
+            stateOptions = new String[optionsLength];
+            stateTransitions = new String[optionsLength];
+
+            for (int i = 0; i < optionsLength; i++) {
+                stateOptions[i] = optionsArray.getString(i);
+                stateTransitions[i] = transitionsArray.getString(i);
+            }
+        } catch (Exception e) {
+            System.err.println("Error: error getting state information.");
+            e.printStackTrace();
+        }
+        return new GameState(context, stateOptions, stateTransitions);
+    }
+
 }
